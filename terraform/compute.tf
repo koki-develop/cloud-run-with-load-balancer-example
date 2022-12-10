25ba2406872441cf0fb025a19358a38f4352deeb
koki-develop <kou.pg.0131@gmail.com>
2022-12-10 15:58:34 +0900

    https プロキシを作成

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -34,3 +34,9 @@ resource "google_compute_url_map" "main" {
   name            = "${local.name}-urlmap"
   default_service = google_compute_backend_service.main.id
 }
+
+resource "google_compute_target_https_proxy" "main" {
+  name             = "${local.name}-https-proxy"
+  url_map          = google_compute_url_map.main.id
+  ssl_certificates = [google_compute_managed_ssl_certificate.main.id]
+}
