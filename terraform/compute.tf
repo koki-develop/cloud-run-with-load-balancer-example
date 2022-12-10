bd0cbeff23c625e6ff5252c9b9efc1b75e486c59
koki-develop <kou.pg.0131@gmail.com>
2022-12-10 15:46:22 +0900

    マネージド SSL 証明書を作成

--- terraform/compute.tf
+++ terraform/compute.tf
@@ -1,3 +1,10 @@
 resource "google_compute_global_address" "main" {
   name = "${local.name}-address"
 }
+
+resource "google_compute_managed_ssl_certificate" "main" {
+  name = "${local.name}-cert"
+  managed {
+    domains = [var.domain]
+  }
+}
